b2eae445689584ed7e185af582aca83896bd1629
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-04-16 14:52:53 +0900

    add parseInfixExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -291,3 +291,75 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"-a * b",
+			"((-a)*b)",
+		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a+b)+c)",
+		},
+		{
+			"a + b - c",
+			"((a+b)-c)",
+		},
+		{
+			"a * b * c",
+			"((a*b)*c)",
+		},
+		{
+			"a * b / c",
+			"((a*b)/c)",
+		},
+		{
+			"a + b / c",
+			"(a+(b/c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a+(b*c))+(d/e))-f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3+4)((-5)*5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5>4)==(3<4))",
+		},
+		{
+			"5 < 4 != 3 > 4",
+			"((5<4)!=(3>4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3+(4*5))==((3*1)+(4*5)))",
+		},
+		{
+			"3 + 4 * 5 != 3 * 1 + 4 * 5",
+			"((3+(4*5))!=((3*1)+(4*5)))",
+		},
+	}
+
+	for _, testCase := range tests {
+		l := lexer.New(testCase.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		actual := program.String()
+		if actual != testCase.expected {
+			t.Errorf("expected=%q, got=%q", testCase.expected, actual)
+		}
+	}
+}
