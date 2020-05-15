f3467601986c46d346c689cbd2a87a5fe5b600f9
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-05-15 11:45:29 +0900

    add return value object

--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type Type string
 
 const (
-	IntegerObj = "INTEGER"
-	BooleanObj = "BOOLEAN"
-	NullObj    = "NULL"
+	IntegerObj     = "INTEGER"
+	BooleanObj     = "BOOLEAN"
+	NullObj        = "NULL"
+	ReturnValueObj = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -19,17 +20,24 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() Type      { return IntegerObj }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() Type      { return BooleanObj }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
-func (n *Null) Inspect() string { return "null" }
 func (n *Null) Type() Type      { return NullObj }
+func (n *Null) Inspect() string { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() Type      { return ReturnValueObj }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
