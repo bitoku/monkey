3f224d090a1bed8f1955f3a779e9d132c0775874
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-05-15 12:29:21 +0900

    add return value evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,33 +18,26 @@ func Eval(node ast.Node) object.Object {
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatements(node.Statements)
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	}
 
 	return nil
-}
-
-func evalStatements(statements []ast.Statement) object.Object {
-	var result object.Object
-
-	for _, statement := range statements {
-		result = Eval(statement)
-	}
-
-	return result
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
@@ -138,4 +131,30 @@ func isTruthy(obj object.Object) bool {
 		return false
 	}
 	return true
+}
+
+func evalProgram(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+	return result
+}
+
+func evalBlockStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.ReturnValueObj {
+			return result
+		}
+	}
+	return result
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -151,3 +151,27 @@ func testNullObject(t *testing.T, obj object.Object) bool {
 	}
 	return true
 }
+
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"return 10;", 10},
+		{"return 10; 9;", 10},
+		{"return 2 * 5; 9;", 10},
+		{"9; return 2 * 5; 9;", 10},
+		{`
+if (10 > 1) {
+  if (10 > 1) {
+    return 10;
+  }
+  return 1;
+}`, 10},
+	}
+
+	for _, testCase := range tests {
+		evaluated := testEval(testCase.input)
+		testIntegerObject(t, evaluated, testCase.expected)
+	}
+}
